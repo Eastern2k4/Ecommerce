4104f598b189240c707e787f3c2712152ac2361c
Nguyễn Nam <namnguyen@MacBook-Air-cua-Nguyen-3.local>
2025-10-18 15:43:18 +0700

    Initial project setup with Spring Boot and 3-layer structure

---
+++ src/main/java/com/example/demo/domain/model/Role.java
@@ -0,0 +1,46 @@
+package com.example.demo.domain.model;
+
+//import java.util.Objects;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id; // TINYINT UNSIGNED → fits in Integer
+
+    @Column(name = "name", nullable = false, unique = true, length = 32)
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(Integer id) {
+        this.id = id;
+    }
+
+    public Role(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
